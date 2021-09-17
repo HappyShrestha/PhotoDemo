f8ba451e793fa46675041f685e7ff46e5ec3d3b0
Happy Shrestha <happyshrest@gmail.com>
2021-09-17 18:56:31 -0400

    Create and Demonstrate MattedPhoto and FramedPhoto Classes

--- photo.js
+++ photo.js
@@ -30,3 +30,50 @@ console.log(photo1.toString());
 
 let photo2 = new Photo(10,12);
 console.log(photo2.toString());
+
+
+class MattedPhoto extends Photo {
+    constructor(width, height, color){
+        super(width, height);
+        this.color = color;
+    }
+
+    price() {
+        const addedPrice = 10;
+        return super.price() + addedPrice;
+    }
+    
+    toString(){
+        return `This is a ${this.width} by ${this.height} matted ${this.color} photo and it costs ${this.price()}`;
+    }
+}
+
+
+class FramedPhoto extends Photo {
+    
+    constructor(width, height, material, style){
+        super(width, height);
+        this.material = material;
+        this.style = style;
+    }
+
+    price() {
+        const addedPrice = 25;
+        return super.price() + addedPrice;
+    }
+    
+    toString(){
+        return `This is a ${this.width} by ${this.height} ${this.material} framed photo. The style is ${this.style} and it costs ${this.price()}`;
+    }
+}
+let mattedPhoto1 = new MattedPhoto(8, 10, 'blue');
+console.log(mattedPhoto1.toString());
+
+let mattedPhoto2 = new MattedPhoto(20, 30, 'red');
+console.log(mattedPhoto2.toString());
+
+let framedPhoto1 = new FramedPhoto(8, 10, 'metal', 'modern');
+console.log(framedPhoto1.toString());
+
+let framedPhoto2 = new FramedPhoto(20, 30, 'wood', 'traditional');
+console.log(framedPhoto2.toString());
