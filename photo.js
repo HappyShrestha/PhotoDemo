c91cf0946121210c1cef44e56d5c68614c1b19af
Happy Shrestha <happyshrest@gmail.com>
2021-09-17 19:13:37 -0400

    Convert to using module feature

--- photo.js
+++ photo.js
@@ -1,4 +1,4 @@
-class Photo {
+export class Photo {
   constructor(width = 8, height = 10) {
     this.width = width;
     this.height = height;
@@ -9,71 +9,56 @@ class Photo {
 
     if (this.width === 8 && this.height === 10) {
       finalPrice = 4;
-    }
-    else {
-        if (this.width === 10 && this.height === 12) {
+    } else {
+      if (this.width === 10 && this.height === 12) {
         finalPrice = 6;
-      }else{
-          finalPrice =10;
+      } else {
+        finalPrice = 10;
       }
-      }
-      return finalPrice;
     }
+    return finalPrice;
+  }
 
-    toString(){
-        return`This is a ${this.width} by ${this.height} photo and costs ${this.price()}`
-    
-    }
-}
-let photo1 = new Photo(8,10);
-console.log(photo1.toString());
-
-let photo2 = new Photo(10,12);
-console.log(photo2.toString());
-
-
-class MattedPhoto extends Photo {
-    constructor(width, height, color){
-        super(width, height);
-        this.color = color;
-    }
-
-    price() {
-        const addedPrice = 10;
-        return super.price() + addedPrice;
-    }
-    
-    toString(){
-        return `This is a ${this.width} by ${this.height} matted ${this.color} photo and it costs ${this.price()}`;
-    }
+  toString() {
+    return `This is a ${this.width} by ${
+      this.height
+    } photo and costs ${this.price()}`;
+  }
 }
 
+export class MattedPhoto extends Photo {
+  constructor(width, height, color) {
+    super(width, height);
+    this.color = color;
+  }
 
-class FramedPhoto extends Photo {
-    
-    constructor(width, height, material, style){
-        super(width, height);
-        this.material = material;
-        this.style = style;
-    }
+  price() {
+    const addedPrice = 10;
+    return super.price() + addedPrice;
+  }
 
-    price() {
-        const addedPrice = 25;
-        return super.price() + addedPrice;
-    }
-    
-    toString(){
-        return `This is a ${this.width} by ${this.height} ${this.material} framed photo. The style is ${this.style} and it costs ${this.price()}`;
-    }
+  toString() {
+    return `This is a ${this.width} by ${this.height} matted ${
+      this.color
+    } photo and it costs ${this.price()}`;
+  }
 }
-let mattedPhoto1 = new MattedPhoto(8, 10, 'blue');
-console.log(mattedPhoto1.toString());
 
-let mattedPhoto2 = new MattedPhoto(20, 30, 'red');
-console.log(mattedPhoto2.toString());
+export class FramedPhoto extends Photo {
+  constructor(width, height, material, style) {
+    super(width, height);
+    this.material = material;
+    this.style = style;
+  }
 
-let framedPhoto1 = new FramedPhoto(8, 10, 'metal', 'modern');
-console.log(framedPhoto1.toString());
+  price() {
+    const addedPrice = 25;
+    return super.price() + addedPrice;
+  }
 
-let framedPhoto2 = new FramedPhoto(20, 30, 'wood', 'traditional');
-console.log(framedPhoto2.toString());
+  toString() {
+    return `This is a ${this.width} by ${this.height} ${
+      this.material
+    } framed photo. The style is ${this.style} and it costs ${this.price()}`;
+  }
+}
